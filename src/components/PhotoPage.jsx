05a1d18ae2f8579147133e9e64f8789e6ca5c35d
FastedNgoding <nazriainurrahman@gmail.com>
2026-06-29 11:05:54 +0700

    Update skaniga-portrait

--- src/components/PhotoPage.jsx
+++ src/components/PhotoPage.jsx
@@ -333,7 +333,7 @@ export default function PhotoPage({ onComplete, onBack }) {
         <button
           onClick={onBack}
           disabled={autoRunning}
-          className="pointer-events-auto flex items-center justify-center w-10 h-10 rounded-full bg-white/10 backdrop-blur-md border border-white/20 text-white transition-all hover:bg-white/20 active:scale-90 disabled:opacity-30 ml-4 mt-4"
+          className="pointer-events-auto flex items-center justify-center w-10 h-10 rounded-full bg-white/10 backdrop-blur-md border border-white/20 text-white transition-all hover:bg-white/20 active:scale-90 disabled:opacity-30 ml-4 mt-5"
         >
           <ArrowBigLeft size="20" />
         </button>
@@ -353,7 +353,7 @@ export default function PhotoPage({ onComplete, onBack }) {
         <div className="w-10 h-10" /> {/* Balancer */}
       </div>
 
-      <div className="flex-1 relative w-full h-full flex items-center justify-center bg-[#050505] p-2 md:p-6 pb-24 md:pb-6">
+      <div className="flex-1 relative w-full h-full flex items-center justify-center bg-[#050505] p-5 md:p-6 pb-24 md:pb-6">
         <div className="flex flex-col md:flex-row items-center justify-center gap-6 w-full max-w-[1400px] h-full max-h-[85vh]">
           
           <div className="relative w-full h-full mx-auto md:rounded-[40px] overflow-hidden flex flex-1 items-center justify-center shadow-2xl">
